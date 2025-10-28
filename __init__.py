f9aabb13886d4ffb94972f4642cb85422ff82421
wallen0322 <496727593@qq.com>
2025-10-28 21:15:09 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,15 @@
-from .nodes_wan_svi import WanSVIImageToVideo, SVIExtractLastFrames, SVIExtractLastImages, SVIPaddingControl, WanSVIExtension
+from .nodes_wan_svi import WanSVIImageToVideo, SVIExtractLastFrames, SVIExtractLastImages, WanSVIExtension
 
 NODE_CLASS_MAPPINGS = {
     "WanSVIImageToVideo": WanSVIImageToVideo,
     "SVIExtractLastFrames": SVIExtractLastFrames,
     "SVIExtractLastImages": SVIExtractLastImages,
-    "SVIPaddingControl": SVIPaddingControl,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "WanSVIImageToVideo": "WAN SVI Image to Video",
     "SVIExtractLastFrames": "SVI Extract Last Frames",
     "SVIExtractLastImages": "SVI Extract Last Images",
-    "SVIPaddingControl": "SVI Padding Control",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
